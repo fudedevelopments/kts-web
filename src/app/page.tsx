cdde75b1fac21928fa541cc448df349eb47cd573
ROHITH-T893 <rrminiature69@gmail.com>
2025-09-11 20:59:30 +0530

    overflow issues solved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,42 +29,35 @@ export default function Home() {
     <>
       <AnimationPerformanceOptimizer />
       {showLoader && <PageLoader />}
-      <div className={`transition-opacity duration-500 ${showLoader ? 'opacity-0' : 'opacity-100'}`}>
+      <div className={`transition-opacity duration-500 ${showLoader ? 'opacity-0' : 'opacity-100'} overflow-x-hidden`}>
         <Header />
         <main>
           <Hero />
-          
           {/* Impact Section - Slide up with bounce effect */}
           <AnimatedSection animation="slideInUp" delay={100} duration={1000}>
             <ImpactSection />
           </AnimatedSection>
-          
           {/* Focus Areas - Slide from left */}
           <AnimatedSection animation="fadeLeft" delay={150} duration={900}>
             <FocusAreas />
           </AnimatedSection>
-          
           {/* Vision Mission - Scale in with elegance */}
           <AnimatedSection animation="scaleIn" delay={200} duration={800}>
             <VisionMission />
           </AnimatedSection>
-          
           {/* Achievements - Slide from right */}
           <AnimatedSection animation="fadeRight" delay={180} duration={900}>
             <Achievements />
           </AnimatedSection>
-          
           {/* Testimonials - Zoom in effect */}
           <AnimatedSection animation="zoomIn" delay={120} duration={1000}>
             <TestimonialsSection />
           </AnimatedSection>
-          
           {/* Latest Updates - Fade up with delay */}
           <AnimatedSection animation="fadeUp" delay={250} duration={800}>
             <LatestUpdates />
           </AnimatedSection>
         </main>
-        
         {/* Footer - Simple fade up */}
         <AnimatedSection animation="fadeUp" delay={100} duration={600}>
           <Footer />
